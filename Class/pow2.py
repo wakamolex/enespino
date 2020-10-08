82acf8af88cc797e238e9d15351375cad21d68d2
wakamolex <58450376+wakamolex@users.noreply.github.com>
2020-10-07 20:28:13 -0500

    Add files via upload

---
+++ Class/pow2.py
@@ -0,0 +1,15 @@
+class Pow2:
+    def __init__(self, dato=1):
+        self.dato=dato
+        
+    def pow2(self):
+    	resp = self.dato**2
+    	return resp
+
+    def mod2(self):
+    	mod = self.dato%2
+    	return mod
+
+    def pow3(self):
+    	pw3 = self.dato**3
+    	return pw3
